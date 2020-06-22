1de6bb37236a24eaa02faa5d5f082605af894c52
jeanjeandelastreet <jean.frenoi59@laposte.net>
2020-06-22 22:19:11 +0200

    Layout Tickets

--- src/main/kotlin/fabriqueurTicketsPackage/ticket/Plat.kt
+++ src/main/kotlin/fabriqueurTicketsPackage/ticket/Plat.kt
@@ -1,12 +1,13 @@
 package fabriqueurTicketsPackage.ticket
 
 import javafx.collections.FXCollections
+import javafx.collections.ObservableList
 
 
 data class Plat (val entree: String?, val feculent: String?, val legume: String?, val viande: String?, val laitage: String?, val dessert: String?, val cafe: String?){
     companion object{
-        val possibleSuggestionsEntree = FXCollections.observableArrayList<String>("Carrotes", "Salade" , "Tomate")
-        val possibleSuggestionsCafe = FXCollections.observableArrayList<String>("Deca", "Normal" , "Thé" , "Long")
+        val possibleSuggestionsEntree: ObservableList<String> = FXCollections.observableArrayList<String>("Carrotes", "Salade" , "Tomate")
+        val possibleSuggestionsCafe: ObservableList<String> = FXCollections.observableArrayList<String>("Deca", "Normal" , "Thé" , "Long")
     }
     fun print() = toString()
 }
